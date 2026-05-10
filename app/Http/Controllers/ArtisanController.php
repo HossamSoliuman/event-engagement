cca50812fe82d67fa8a08ff0921d2b9170509d67
Hossam Soliuman <hossamsoliuman@gmail.com>
2026-05-10 22:21:11 +0300

    refactor: remove artisan

--- app/Http/Controllers/ArtisanController.php
+++ app/Http/Controllers/ArtisanController.php
@@ -6,31 +6,7 @@ class ArtisanController extends Controller
 {
     public function __invoke()
     {
-        $source = storage_path('app/public');
-        $dest   = base_path('htdocs/storage');
-
-        if (!file_exists($source)) {
-            return 'Source folder does not exist.';
-        }
-
-        $this->copyFolder($source, $dest);
-
-        return 'Done.';
+       
     }
 
-    private function copyFolder(string $source, string $dest): void
-    {
-        if (!file_exists($dest)) {
-            mkdir($dest, 0755, true);
-        }
-
-        foreach (scandir($source) as $item) {
-            if ($item === '.' || $item === '..') continue;
-
-            $from = $source . DIRECTORY_SEPARATOR . $item;
-            $to   = $dest   . DIRECTORY_SEPARATOR . $item;
-
-            is_dir($from) ? $this->copyFolder($from, $to) : copy($from, $to);
-        }
-    }
 }
