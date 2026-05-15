7d391bacf65f44bbf6c523557500d026bddd98e4
Hossam Soliuman <hossamsoliuman@gmail.com>
2026-05-16 02:32:34 +0300

    test update

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -264,7 +264,7 @@
 <body>
     <div class="wrap">
         <div class="logo">
-            <h1> EventBomb</h1>
+            <h1> EventBomb up</h1>
             <p>Admin Console · Secure Access</p>
         </div>
         <div class="card">
